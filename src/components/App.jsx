22bcaa8d774169bf5920a5102d1877f892762bae
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-11-09 22:57:31 +0100

    Add main routes and navigation
    I added two main routes and navigation links with styled-components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home } from './Home/Home';
+import { Movies } from './Movies/Movies';
+import styled from 'styled-components';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: red;
+  }
+`;
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <nav>
+        <StyledLink to="/" end>
+          HOME
+        </StyledLink>
+        <StyledLink to="/movies">MOVIES</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
