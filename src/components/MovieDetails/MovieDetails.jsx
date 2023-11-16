7a9b1b07a90de70ab39a9d160599ea82ccb5a473
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-11-16 21:47:04 +0100

    Fix fetching movie details bug
    I fixed a bug related to fetching movie details functionality.

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,31 +1,41 @@
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
   const [details, setDetails] = useState({});
   const poster = `https://www.themoviedb.org/t/p/w500${details.poster_path}`;
-  axios
-    .get(`https://api.themoviedb.org/3/movie/${movieId}`, {
-      params: {
-        language: 'en-US',
-        api_key: 'c90cdec037818042646f6ab3cec9ea66',
-      },
-      headers: {
-        accept: 'application/json',
-      },
-    })
-    .then(movies => {
-      setDetails(movies.data);
-    });
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}`,
+          {
+            params: {
+              language: 'en-US',
+              api_key: 'c90cdec037818042646f6ab3cec9ea66',
+            },
+            headers: {
+              accept: 'application/json',
+            },
+          }
+        );
+        setDetails(response.data);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+    fetchMovieDetails();
+  }, [movieId]);
 
   return (
     <div>
       <h2>
-        {details.title} {`(${details.release_date.slice(0, 4)})`}
+        {details.title} {`(${details.release_date?.slice(0, 4)})`}
       </h2>
-      <p>User Score: {details.popularity.toFixed(1)}%</p>
+      <p>User Score: {details.popularity?.toFixed(1)}%</p>
       <img src={poster} alt="" />
     </div>
   );
