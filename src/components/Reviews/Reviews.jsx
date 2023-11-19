8959acf6debdf3149a65aaa53630be5d09f6ee75
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-11-19 17:53:37 +0100

    Add fetching reviews
    I added functionality for fetching movie reviews and I added styles to Review component.

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,48 @@
+import axios from 'axios';
+import style from './reviews.module.css';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 export const Reviews = () => {
-  return <div>REVIEWS</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/reviews`,
+          {
+            params: {
+              language: 'en-US',
+              api_key: 'c90cdec037818042646f6ab3cec9ea66',
+            },
+            headers: {
+              accept: 'application/json',
+            },
+          }
+        );
+        setReviews(response.data.results);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+      }
+    };
+    fetchReviews();
+  }, [movieId]);
+
+  return (
+    <>
+      <div className={style.separator}></div>
+      <ul className={style.reviews}>
+        {reviews?.map(review => {
+          return (
+            <li className={style.item}>
+              <h2>Author: {review.author}</h2>
+              <p>{review.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
